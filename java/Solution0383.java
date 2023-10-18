a288c87e2450997a441420ee18b31f38e9b82fce
wangmz <w2639764449@hotmail.com>
2023-10-18 09:46:58 +0800

    classical interview questions - hash table

--- java/Solution0383.java
+++ java/Solution0383.java
@@ -11,3 +11,21 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(magazine.length() < ransomNote.length()) return false;
+        // mapping
+        int[] map = new int[26];
+        for(int i = 0; i < magazine.length(); i++) {
+            map[magazine.charAt(i)-'a']++;
+        }
+        for(int i = 0; i < ransomNote.length(); i++) {
+            int key = ransomNote.charAt(i)-'a';
+            if(map[key] > 0) {
+                map[key] --;
+            } else return false;
+        }
+        return true;
+    }
+}
